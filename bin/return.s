057755d0cda84bb616be6706bebbf95f36e77251
armenalaray <armenalaray@gmail.com>
2025-04-18 19:40:17 -0600

    -

--- bin/return.s
+++ bin/return.s
@@ -1,20 +1,21 @@
-.globl	main
-
-/*
-este es el address de movl solo tiene que ser simbolico, no tiene que ser real!:
-ATT
-
-mov longwords 32 bits eax
-quadwords 64 bits rax
-*/
-
+    .globl main
 main:
+    # prologue
+    # -2 2 1
 
-movl	$2, %eax
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $8, %rsp
+    
+    # store 2 in rbp -4
+    movl $2, -4(%rbp)
+    
+    3 negl -4(%rbp)
+    4 movl -4(%rbp), %r10d
+    5 movl %r10d, -8(%rbp)
+    6 notl -8(%rbp)
+    7 movl -8(%rbp), %eax
 
-/*
-retq because it returns into 64 bit address
-*/
-
-ret
-	
+    movq %rbp, %rsp
+    popq %rbp
+    ret
--- build.sh
+++ build.sh
@@ -2,15 +2,17 @@
 #python3 -m pdb "$(pwd)"/code/cd.py "$(pwd)"/test/test.c
 
 #compile
+
 python3 "$(pwd)"/code/cd.py "$(pwd)"/test/test.c
+exit_status=$?
+echo $exit_status
 
-#chmod 777 /home/alejandro/CComp/test/test
-ls -la /home/alejandro/CComp/test/test
-
-#run
-./test/test
-#check out code
-echo $?
+number=0
+if [ $exit_status -eq $number ]; then
+    ls -la /home/alejandro/CComp/test/test
+    ./test/test
+    echo $?
+fi
 
 #python3 "$(pwd)"/code/test.py
 
