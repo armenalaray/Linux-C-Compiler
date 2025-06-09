abda951d1618b9f97fdbaedf62256efcc751a848
armenalaray <armenalaray@gmail.com>
2025-06-08 20:51:58 -0600

    Finished Type Checker

--- test_/test.c
+++ test_/test.c
@@ -1,10 +1,5 @@
-/* This is adjusted to
- *   int f(int (*arr)[3])
- */
-int f(int arr[2][3]);
-
-/* This is adjusted to
- *   int f(int (*arr)[4])
- * so it conflicts with the previous declaration
- */
-int f(int arr[2][4]);
+int main(void) {
+    int a = 3;
+    // one operand in a subscript operation must be a pointer
+    return a[4];
+}
